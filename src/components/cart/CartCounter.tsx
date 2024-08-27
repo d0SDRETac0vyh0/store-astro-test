fb2618c49217fdf13ce4e7a266e3ad685e464d5e
Cristian <cristianvillalobos666@gmail.com>
2024-08-27 18:59:57 -0400

    beta 0..5 snipcart

--- src/components/cart/CartCounter.tsx
+++ src/components/cart/CartCounter.tsx
@@ -5,21 +5,12 @@ import { CartCookiesClient } from '@/utils';
 
 export const CartCounter=()=>{
 
-    const $itemsInCart = useStore(itemsInCart)
-
-    useEffect(() => {
-        const cart = CartCookiesClient.getCart();
-        itemsInCart.set(cart.length);
-      }, []);
+    
 
       return (
-        <a href="/cart" className="relative inline-block">
-          {$itemsInCart > 0 && (
-            <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5">
-              {$itemsInCart}
-            </span>
-          )}
-    
+        <a href="" className="relative inline-block snipcart-checkout">
+
+
     <svg xmlns="http://www.w3.org/2000/svg" 
             width="2em" 
             height="2em" 
