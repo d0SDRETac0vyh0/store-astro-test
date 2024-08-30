8eec256a65315f5cbb0f1e205b7f36c2719a55ac
Cristian <cristianvillalobos666@gmail.com>
2024-08-30 00:03:45 -0400

    web bitals 0.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
 import db from "@astrojs/db";
 import auth from "auth-astro";
-
 import react from "@astrojs/react";
+
+import webVitals from "@astrojs/web-vitals";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), auth(), react()],
+  integrations: [tailwind(), db(), auth(), react(), webVitals()],
   output: "server",
   adapter: netlify(),
   experimental: {
